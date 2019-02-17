510ccb2fdad94cca44966e3ab992268bfd7db1e2
Victoria MG <victoriamarcosg7@gmail.com>
2019-02-17 20:38:05 +0100

    var entities - Proceso (III)

--- js/game.js
+++ js/game.js
@@ -10,18 +10,18 @@ $(window).load(function onload() {
     const GameState = Object.freeze({
         INTRO: {}, // – intro: El juego se desplazará alrededor del nivel para mostrarle al
         //                  usuario todo lo que hay en él.
-        
+
         NEXT_HERO: {}, // – load-next-hero: El juego comprueba si hay otro héroe que cargar
         //                      en la honda. El juego termina cuando ya no hay héroes o los villanos
         //                      han sido destruidos.
-        
+
         WAITING: {}, // – wait-for-firing: El juego se desplaza hacia el área donde está la
         //                  honda, en espera de que el usuario dispare al héroe.
-        
+
         AIMING: {}, // – firing: Tiene lugar cuando el usuario pulsa el héroe, pero antes de
         //                  que libere el botón del ratón. Se prepara el ángulo de tiro y la altura
         //                  a la que se lanzará el héroe.
-        
+
         FIRED: {} // – fired: Sucede después de que el usuario libera el botón del ratón,
         //              entonces se habrá lanzado al héroe y el motor de la física actuará
         //              sobre todo, mientras el usuario sigue la trayectoria del héroe.
@@ -60,13 +60,13 @@ $(window).load(function onload() {
             switch (this.mode) {
                 case GameState.INTRO:
                     if (this.panTo(700)) {
-                        this.mode=GameState.NEXT_HERO;
+                        this.mode = GameState.NEXT_HERO;
                     }
                     break;
                 case GameState.WAITING:
-                    if(this.mouse.dragging){
-                        panTo(this.mouse.x+this.offsetLeft);
-                    }else{
+                    if (this.mouse.dragging) {
+                        panTo(this.mouse.x + this.offsetLeft);
+                    } else {
                         this.panTo(this.slingshotX);
                     }
                     break;
@@ -82,22 +82,22 @@ $(window).load(function onload() {
         }
 
 
-        panTo(pos){
-            if (Math.abs(pos-this.offsetLeft-this.canvas.width/4) > 0 &&
-                    this.offsetLeft <= this.maxOffset &&
-                    this.offsetLeft>=this.minOffset) {
-                let deltaX = Math.round((pos-this.offsetLeft-this.canvas.width/4)/2);
-                if(deltaX && Math.abs(deltaX)>this.maxSpeed){
-                    deltaX = this.maxSpeed*Math.abs(deltaX)/deltaX;
+        panTo(pos) {
+            if (Math.abs(pos - this.offsetLeft - this.canvas.width / 4) > 0 &&
+                this.offsetLeft <= this.maxOffset &&
+                this.offsetLeft >= this.minOffset) {
+                let deltaX = Math.round((pos - this.offsetLeft - this.canvas.width / 4) / 2);
+                if (deltaX && Math.abs(deltaX) > this.maxSpeed) {
+                    deltaX = this.maxSpeed * Math.abs(deltaX) / deltaX;
                 }
                 this.offsetLeft += deltaX;
-            }else{
+            } else {
                 return true;
             }
             if (this.offsetLeft < this.minOffset) {
                 this.offsetLeft = this.minOffset;
                 return true;
-            } else if(this.offsetLeft>this.maxOffset){
+            } else if (this.offsetLeft > this.maxOffset) {
                 this.offsetLeft = this.maxOffset;
                 return true;
             }
@@ -187,6 +187,7 @@ $(window).load(function onload() {
             this.dragging = false;
         }
     }
+
     class Levels {
 
         constructor() {
@@ -254,7 +255,7 @@ $(window).load(function onload() {
             }
             this.soundFileExt = this.oggSupport ? ".ogg" :
                 this.mp3Support ? ".mp3" :
-                undefined
+                    undefined
         }
 
         loadImage(url) {
@@ -329,3 +330,80 @@ $(window).load(function onload() {
             clearTimeout(id);
         };
 }());
+//Definimos las entidades
+var entities = {
+    definitions: {
+        "glass": {
+            fullHealth: 200,
+            density: 2.4,
+            friction: 0.4,
+            restitution: 0.15,
+        },
+        "wood": {
+            fullHealth: 900,
+            density: 0.7,
+            friction: 0.4,
+            restitution: 0.4,
+        },
+        "dirt": {
+            density: 3.0,
+            friction: 1.5,
+            restitution: 0.2,
+        },
+        "burger": {
+            shape: "circle",
+            fullHealth: 40,
+            radius: 25,
+            density: 1,
+            friction: 0.5,
+            restitution: 0.4,
+        },
+        "sodacan": {
+            shape: "rectangle",
+            fullHealth: 80,
+            width: 40,
+            height: 60,
+            density: 1,
+            friction: 0.5,
+            restitution: 0.7,
+        },
+        "fries": {
+            shape: "rectangle",
+            fullHealth: 50,
+            width: 40,
+            height: 50,
+            density: 1,
+            friction: 0.5,
+            restitution: 0.6,
+        },
+        "apple": {
+            shape: "circle",
+            radius: 25,
+            density: 1.5,
+            friction: 0.5,
+            restitution: 0.4,
+        },
+        "orange": {
+            shape: "circle",
+            radius: 25,
+            density: 1.5,
+            friction: 0.5,
+            restitution: 0.4,
+        },
+        "strawberry": {
+            shape: "circle",
+            radius: 15,
+            density: 2.0,
+            friction: 0.5,
+            restitution: 0.4,
+        }
+    },
+    //TODO: Añadir aqui nuevas entidades
+    // Tomar la entidad crear un cuerpo Box2D y añadirlo al mundo
+    create: function (entity) {
+          //TODO: completar
+    },
+    // Coge la posicion y la entidad y la dibuja
+    draw: function (entity, position, angle) {
+    //TODO: completar
+    }
