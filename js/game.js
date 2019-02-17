ab3b557cbe08fed24070edad91aac579e5dfae58
Victoria MG <victoriamarcosg7@gmail.com>
2019-02-17 21:36:34 +0100

    Añadidas entidades a los niveles

--- js/game.js
+++ js/game.js
@@ -330,6 +330,57 @@ $(window).load(function onload() {
             clearTimeout(id);
         };
 }());
+var levels = {
+    // Nivel de datos. Tiene un array con info acerca de cada nivel
+    data: [
+        { // Primer nivel
+            foreground: 'desert-foreground',
+            background: 'clouds-background',
+            entities: [
+                {type: "ground", name: "dirt", x: 500, y: 440, width: 1000, height: 20, isStatic: true},
+                {type: "ground", name: "wood", x: 185, y: 390, width: 30, height: 80, isStatic: true},
+
+                {type: "block", name: "wood", x: 520, y: 380, angle: 90, width: 100, height: 25},
+                {type: "block", name: "glass", x: 520, y: 280, angle: 90, width: 100, height: 25},
+                {type: "villain", name: "burger", x: 520, y: 205, calories: 590},
+
+                {type: "block", name: "wood", x: 620, y: 380, angle: 90, width: 100, height: 25},
+                {type: "block", name: "glass", x: 620, y: 280, angle: 90, width: 100, height: 25},
+                {type: "villain", name: "fries", x: 620, y: 205, calories: 420},
+
+                {type: "hero", name: "orange", x: 80, y: 405},
+                {type: "hero", name: "apple", x: 140, y: 405},
+            ]
+        },
+        {   // Segundo nivel
+            foreground: 'Egip_BG',
+            background: 'Egip_BG',
+            entities: [
+                {type: "ground", name: "dirt", x: 500, y: 440, width: 1000, height: 20, isStatic: true},
+                {type: "ground", name: "wood", x: 185, y: 390, width: 30, height: 80, isStatic: true},
+
+                {type: "block", name: "wood", x: 820, y: 380, angle: 90, width: 100, height: 25},
+                {type: "block", name: "wood", x: 720, y: 380, angle: 90, width: 100, height: 25},
+                {type: "block", name: "wood", x: 620, y: 380, angle: 90, width: 100, height: 25},
+                {type: "block", name: "glass", x: 670, y: 317.5, width: 100, height: 25},
+                {type: "block", name: "glass", x: 770, y: 317.5, width: 100, height: 25},
+
+                {type: "block", name: "glass", x: 670, y: 255, angle: 90, width: 100, height: 25},
+                {type: "block", name: "glass", x: 770, y: 255, angle: 90, width: 100, height: 25},
+                {type: "block", name: "wood", x: 720, y: 192.5, width: 100, height: 25},
+
+                {type: "villain", name: "burger", x: 715, y: 155, calories: 590},
+                {type: "villain", name: "fries", x: 670, y: 405, calories: 420},
+                {type: "villain", name: "sodacan", x: 765, y: 400, calories: 150},
+
+                {type: "hero", name: "strawberry", x: 30, y: 415},
+                {type: "hero", name: "orange", x: 80, y: 405},
+                {type: "hero", name: "apple", x: 140, y: 405},
+            ]
+        }
+        //TODO:Añadir mas niveles aqui - PONER COMA EN CIERRE ANTERIOR
+    ],
+}
 //Definimos las entidades
 var entities = {
     definitions: {
